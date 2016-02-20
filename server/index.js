73ecd86880d7a34f7f5cf6066d01a01b9708258b
Michal Strzelczyk <mich.strzelczyk@gmail.com>
2016-02-20 12:47:38 -0500

    Hot Module Replacament for development

--- server/index.js
+++ server/index.js
@@ -1,19 +1,42 @@
+import path from 'path';
 import express from 'express';
+import handlebars from 'express-handlebars';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './generated/app';
-import handlebars from 'express-handlebars';
-const path = require('path');
 
 const app = express();
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+
+// View templates
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main',
+  layoutsDir: path.resolve(__dirname, 'views/layouts')
+}));
 app.set('view engine', 'handlebars');
+app.set('views', path.resolve(__dirname, 'views'));
+
+// Static assets
 app.use(express.static(path.resolve(__dirname, '../dist')));
+
+// Routes
 app.get('/', (request, response) => {
+  const initialState = {
+    currentMessage: '',
+    messages: []
+  };
+  const store = createStore((state=initialState) => state);
+  const appContent = ReactDOMServer.renderToString(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
   response.render('app', {
-    app: ReactDOMServer.renderToString(<App />)
+    app: appContent,
+    initialState: JSON.stringify(initialState)
   });
 });
 
-
-app.listen(3000, () => console.log('Server running'));
+export default app;
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
-const path = require('path');
+export const CLIENT_DIR = path.resolve(__dirname, 'client');
+export const SERVER_DIR = path.resolve(__dirname, 'server/generated');
+export const DIST_DIR = path.resolve(__dirname, 'dist');
 
-const CLIENT_DIR = path.resolve(__dirname, 'client');
-const SERVER_DIR = path.resolve(__dirname, 'server/generated');
-const DIST_DIR = path.resolve(__dirname, 'dist');
-
-const loaders = [{
+export const babelLoader = {
   test: /\.js$/,
   include: CLIENT_DIR,
   loader: 'babel-loader',
-  query: {
-    presets: ['es2015', 'react']
-  }
-},
-{
+};
+
+export const cssLoader = {
   test: /\.less$/,
   loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
-}
-];
+};
 
-module.exports = [{
+export const aliases = {
+  components: path.resolve(CLIENT_DIR, 'components'),
+  reducers: path.resolve(CLIENT_DIR, 'reducers'),
+  actions: path.resolve(CLIENT_DIR, 'actions')
+};
+
+export const client = {
   name: 'client',
   target: 'web',
   context: CLIENT_DIR,
@@ -30,18 +32,17 @@ module.exports = [{
     filename: 'bundle.js'
   },
   module: {
-    loaders: loaders
+    loaders: [babelLoader, cssLoader]
   },
   resolve: {
-    alias: {
-      components: path.resolve(CLIENT_DIR, 'components')
-    }
+    alias: aliases
   },
   plugins: [
     new ExtractTextPlugin('bundle.css', {allChunks: true})
   ]
-},
-{
+};
+
+export const server = {
   name: 'server',
   target: 'node',
   context: CLIENT_DIR,
@@ -55,14 +56,14 @@ module.exports = [{
   },
   externals: /^[a-z\-0-9]+$/,
   module: {
-    loaders: loaders
+    loaders: [babelLoader, cssLoader]
   },
   resolve: {
-    alias: {
-      components: path.resolve(CLIENT_DIR, 'components')
-    }
+    alias: aliases
   },
   plugins: [
     new ExtractTextPlugin('[name].css')
   ]
-}];
+};
+
+export default [client, server];
